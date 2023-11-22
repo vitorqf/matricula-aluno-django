40c5bc9b3b167fdf1d6b818b3ebdce02382b16ec
JefersonQueiroga <jefersonqueiroga@gmail.com>
2023-11-22 05:40:26 -0300

    adicionar paginação e filtros

--- aluno/forms.py
+++ aluno/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 from django import forms
-from .models import Aluno
+from .models import Aluno,Curso,Cidade
 
 class AlunoForm(ModelForm):
 
@@ -14,3 +14,14 @@ class AlunoForm(ModelForm):
             'cidade': forms.Select(attrs={'class': 'form-control' }),
             'curso': forms.Select(attrs={'class': 'form-control' })
         }
+
+class AlunoFilterForm(forms.Form):
+    nome = forms.CharField(max_length=150, required=False)
+    cidade = forms.ModelChoiceField(queryset=Cidade.objects.all(), required=False)
+    curso = forms.ModelChoiceField(queryset=Curso.objects.all(), required=False)
+    
+    # adicionar a classe form-control para todos os campos
+    def __init__(self, *args, **kwargs):
+        super(AlunoFilterForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
--- aluno/views.py
+++ aluno/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.shortcuts import render,get_object_or_404,redirect
 from .models import Aluno,Curso,Cidade
-from .forms import AlunoForm
+from .forms import AlunoForm,AlunoFilterForm
 
 
 class AlunoCriar(CreateView):
@@ -32,7 +32,24 @@ class AlunoListar(ListView):
     model = Aluno
     template_name = 'aluno/alunos.html'
     context_object_name = 'alunos'  # Nome da variável a ser usada no template
+    paginate_by = 2
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = self.form
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.form = AlunoFilterForm(self.request.GET or None)
+        if self.form.is_valid():
+            if self.form.cleaned_data.get('nome'):
+                queryset = queryset.filter(nome_aluno__icontains=self.form.cleaned_data['nome'])
+            if self.form.cleaned_data.get('cidade'):
+                queryset = queryset.filter(cidade=self.form.cleaned_data['cidade'])
+            if self.form.cleaned_data.get('curso'):
+                queryset = queryset.filter(curso=self.form.cleaned_data['curso'])
+        return queryset
 
 class IndexView(TemplateView):
     template_name = "aluno/index.html"
